3901eff6bb1424282bb945f94dce23309c17da11
q_h <q_h@inbox.ru>
2019-09-16 12:35:20 +0300

    build: tweaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,30 +4,21 @@ module.exports = {
     es6: true,
     node: true
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
-    project: "./tsconfig.json"
+    sourceType: 'module',
+    project: './tsconfig.json'
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:prettier/recommended",
-    "prettier",
-    "prettier/@typescript-eslint"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint'
   ],
-  plugins: ["@typescript-eslint", "prettier"],
-  rules: {},
-  overrides: [
-    {
-      files: ["examples/**/*.*"],
-      rules: {
-        "@typescript-eslint/no-var-requires": "off",
-        "no-console": "off"
-      }
-    }
-  ]
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {}
 };
