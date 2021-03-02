1a9ae883c0e370c8e083610f852a820b44db4f7d
q_h <q_h@inbox.ru>
2021-03-02 13:52:56 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,9 +15,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
-    'prettier',
-    'prettier/@typescript-eslint'
+    'plugin:prettier/recommended'
   ],
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {}
