cddbafce0ee3f875d15ab12f584f01e8f41f1be6
kukhariev <q_h@inbox.ru>
2022-09-13 21:28:29 +0300

    style: migrate to rome

--- src/pick.ts
+++ src/pick.ts
@@ -1,5 +1,5 @@
 export const pick = <T, K extends keyof T>(obj: T, whitelist: K[]): Pick<T, K> => {
   const result = {} as Pick<T, K>;
-  whitelist.forEach(key => (result[key] = obj[key]));
+  whitelist.forEach((key) => (result[key] = obj[key]));
   return result;
 };
